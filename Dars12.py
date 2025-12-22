015bceb6fc074bd504dbbf5924e630e134e5bc60
danaboyevnavroz-cpu <danaboyevnavroz@gmail.com>
2025-12-22 15:51:41 +0500

    yangi fayl joylandi

---
+++ Dars12.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 22 10:38:37 2025
+
+@author: User
+"""
+
+##  Lug'atlar ro'yxati
+
+car_0={
+       "model":"lasetti",
+       "rang":"oq",
+       "yil":2018,
+       "narx":13000,
+       "korabka":"avtomat"
+       }
+
+car_1={
+       "model":"nexia 3",
+       "rang":"qora",
+       "yil":2015,
+       "narx":9000,
+       "korabka":"mexanika"
+       }
+
+car_2={
+       "model":"gentra",
+       "rang":"qizil",
+       "yil":2019,
+       "narx":15000,
+       "korabka":"mexanika"
+       }       
+car=car_0
+
+# print(f"{car["model"].title()},"
+#       f"{car["rang"]} rang,"
+#       f"{car["yil"]}-yil,{car["narx"]}$")
+
+# car=car_1
+
+# print(f"{car["model"].title()},"
+#       f"{car["rang"]} rang,"
+#       f"{car["yil"]}-yil,{car["narx"]}$")
+
+# car=car_2
+
+# print(f"{car["model"].title()},"
+#       f"{car["rang"]} rang,"
+#       f"{car["yil"]}-yil,{car["narx"]}$")
+
+
+cars=[car_0,car_1,car_2]
+# for car in cars:
+#     print(f"{car["model"].title()},"
+#           f"{car["rang"]} rang,"
+#           f"{car["yil"]}-yil,{car["narx"]}$")
+
+# print(cars)
+# print(cars[0]["model"])
+# print(cars[1]["rang"])
+# print(f"{cars[2]["rang"].title()} "
+#       f"{cars[2]["model"]}")
+
+# malibus=[]
+# for n in range(10):
+#     new_car={
+#              "model":"malibu",
+#              "rang":None,       # noaniq rang
+#              "yil":2020,
+#              "narx":None,
+#              "km":0,
+#              "korabka":"avto"
+#             }
+#     malibus.append(new_car)
+# for malibu in malibus:
+#     print(malibu)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
