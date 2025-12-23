97cccdd2fe295a5b93e3594a57ac1a04a545f65a
danaboyevnavroz-cpu <danaboyevnavroz@gmail.com>
2025-12-23 13:44:53 +0500

    Dars-12 yangilandi

--- Dars12.py
+++ Dars12.py
@@ -7,30 +7,30 @@ Created on Mon Dec 22 10:38:37 2025
 
 ##  Lug'atlar ro'yxati
 
-car_0={
-       "model":"lasetti",
-       "rang":"oq",
-       "yil":2018,
-       "narx":13000,
-       "korabka":"avtomat"
-       }
+# car_0={
+#        "model":"lasetti",
+#        "rang":"oq",
+#        "yil":2018,
+#        "narx":13000,
+#        "korabka":"avtomat"
+#        }
 
-car_1={
-       "model":"nexia 3",
-       "rang":"qora",
-       "yil":2015,
-       "narx":9000,
-       "korabka":"mexanika"
-       }
+# car_1={
+#        "model":"nexia 3",
+#        "rang":"qora",
+#        "yil":2015,
+#        "narx":9000,
+#        "korabka":"mexanika"
+#        }
 
-car_2={
-       "model":"gentra",
-       "rang":"qizil",
-       "yil":2019,
-       "narx":15000,
-       "korabka":"mexanika"
-       }       
-car=car_0
+# car_2={
+#        "model":"gentra",
+#        "rang":"qizil",
+#        "yil":2019,
+#        "narx":15000,
+#        "korabka":"mexanika"
+#        }       
+# car=car_0
 
 # print(f"{car["model"].title()},"
 #       f"{car["rang"]} rang,"
@@ -49,7 +49,7 @@ car=car_0
 #       f"{car["yil"]}-yil,{car["narx"]}$")
 
 
-cars=[car_0,car_1,car_2]
+# cars=[car_0,car_1,car_2]
 # for car in cars:
 #     print(f"{car["model"].title()},"
 #           f"{car["rang"]} rang,"
@@ -61,23 +61,37 @@ cars=[car_0,car_1,car_2]
 # print(f"{cars[2]["rang"].title()} "
 #       f"{cars[2]["model"]}")
 
-malibus=[]
-for n in range(10):
-    new_car={
-             "model":"malibu",
-             "rang":None,       # noaniq rang
-             "yil":2020,
-             "narx":None,
-             "km":0,
-             "korabka":"avto"
-            }
-    malibus.append(new_car)
-# for malibu in malibus:
-#     print(malibu)
+# malibus=[]
+# for n in range(10):
+#     new_car={
+#              "model":"malibu",
+#              "rang":None,       # noaniq rang
+#              "yil":2020,
+#              "narx":None,
+#              "km":0,
+#              "korabka":"avto"
+#             }
+#     malibus.append(new_car)
+# # for malibu in malibus:
+# #     print(malibu)
     
-for malibu in malibus[:3]:
-    malibu["rang"]="qizil"
-print(malibus)
+# for malibu in malibus[:3]:
+#     malibu["rang"]="qizil"
+
+
+# for malibu in malibus[3:6]:
+#     malibu["rang"]="qora"
+    
+# for malibu in malibus[6:]:
+#     malibu["rang"]="qizil"
+#     malibu["korabka"]="mexanika"
+    
+# for malibu in malibus:
+#     if malibu["korabka"]=="avto":
+#         malibu["narx"]=40000
+#     else:
+#         malibu["narx"]=35000
+
 
 
 
