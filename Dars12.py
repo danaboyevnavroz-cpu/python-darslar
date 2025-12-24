d107dacc922c47dda591c8ebf4b238be2a0c2214
danaboyevnavroz-cpu <danaboyevnavroz@gmail.com>
2025-12-24 15:52:31 +0500

    Yangi fayllar joylandi

--- Dars12.py
+++ Dars12.py
@@ -7,30 +7,31 @@ Created on Mon Dec 22 10:38:37 2025
 
 ##  Lug'atlar ro'yxati
 
-# car_0={
-#        "model":"lasetti",
-#        "rang":"oq",
-#        "yil":2018,
-#        "narx":13000,
-#        "korabka":"avtomat"
-#        }
+car_0={
+       "model":"lasetti",
+       "rang":"oq",
+       "yil":2018,
+       "narx":13000,
+       "korabka":"avtomat"
+       }
 
-# car_1={
-#        "model":"nexia 3",
-#        "rang":"qora",
-#        "yil":2015,
-#        "narx":9000,
-#        "korabka":"mexanika"
-#        }
+car_1={
+       "model":"nexia 3",
+       "rang":"qora",
+       "yil":2015,
+       "narx":9000,
+       "korabka":"mexanika"
+       }
 
-# car_2={
-#        "model":"gentra",
-#        "rang":"qizil",
-#        "yil":2019,
-#        "narx":15000,
-#        "korabka":"mexanika"
-#        }       
-# car=car_0
+car_2={
+       "model":"gentra",
+       "rang":"qizil",
+       "yil":2019,
+       "narx":15000,
+       "korabka":"mexanika"
+       }       
+
+car=car_0
 
 # print(f"{car["model"].title()},"
 #       f"{car["rang"]} rang,"
@@ -49,48 +50,50 @@ Created on Mon Dec 22 10:38:37 2025
 #       f"{car["yil"]}-yil,{car["narx"]}$")
 
 
-# cars=[car_0,car_1,car_2]
+cars=[car_0,car_1,car_2]
 # for car in cars:
 #     print(f"{car["model"].title()},"
 #           f"{car["rang"]} rang,"
 #           f"{car["yil"]}-yil,{car["narx"]}$")
 
 # print(cars)
-# print(cars[0]["model"])
+# print(cars[0])
 # print(cars[1]["rang"])
 # print(f"{cars[2]["rang"].title()} "
 #       f"{cars[2]["model"]}")
 
-# malibus=[]
-# for n in range(10):
-#     new_car={
-#              "model":"malibu",
-#              "rang":None,       # noaniq rang
-#              "yil":2020,
-#              "narx":None,
-#              "km":0,
-#              "korabka":"avto"
-#             }
-#     malibus.append(new_car)
+malibus=[]
+for n in range(10):
+    new_car={
+             "model":"malibu",
+             "rang":None,       # noaniq rang
+             "yil":2020,
+             "narx":None,
+             "km":0,
+             "korabka":"avto"
+            }
+    malibus.append(new_car)
+
+# print(malibus)
 # # for malibu in malibus:
 # #     print(malibu)
     
-# for malibu in malibus[:3]:
-#     malibu["rang"]="qizil"
+for malibu in malibus[:3]:
+    malibu["rang"]="qizil"
 
 
-# for malibu in malibus[3:6]:
-#     malibu["rang"]="qora"
+for malibu in malibus[3:6]:
+    malibu["rang"]="qora"
     
-# for malibu in malibus[6:]:
-#     malibu["rang"]="qizil"
-#     malibu["korabka"]="mexanika"
+for malibu in malibus[6:]:
+    malibu["rang"]="qizil"
+    malibu["korabka"]="mexanika"
     
-# for malibu in malibus:
-#     if malibu["korabka"]=="avto":
-#         malibu["narx"]=40000
-#     else:
-#         malibu["narx"]=35000
+for malibu in malibus:
+    if malibu["korabka"]=="avto":
+        malibu["narx"]=40000
+    else:
+        malibu["narx"]=35000
 
 
 
